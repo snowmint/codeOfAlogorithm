6133c6de8e031a64774c8863f4e34d7a909bb930
snowmintowo <starlitsky0612@gmail.com>
2018-06-25 22:31:50 +0800

    Create WordFrequencyByMap.cpp
    Use library of c++

---
+++ WordFrequencyByMap.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    map<string, int> mymap;
+    map<string, int>::iterator it;
+    string input, wantToFind;
+    
+    while(cin >> input) {
+        if(input.compare("#") == 0) break;
+    	  it = mymap.find(input);
+    	  if(it != mymap.end()) {
+    		    mymap[input] += 1;
+    	  }
+    	  else {
+    		    mymap[input] = 1;
+    	  }
+    }
+    while(cin >> wantToFind) {
+        if(cin.eof()) break;
+        it = mymap.find(wantToFind);
+      
+        if(it != mymap.end()) {
+            cout << wantToFind << " " << mymap.find(wantToFind)->second << endl;
+    	  }
+    	  else {
+    		    cout << wantToFind << " 0" << endl;
+    	  }
+	  }
+    return 0;
+}
